1b41956429ff9040f54f8ccd8185cf4c40f14032
Jason <likemoody@gmail.com>
2017-10-06 21:37:19 +0200

    Lots of changes: lessons 12th - 25th.

--- gulpfile.js
+++ gulpfile.js
@@ -1,24 +1,9 @@
-var gulp = require('gulp'),
-watch = require('gulp-watch');
+require('./gulp/tasks/styles');
+require('./gulp/tasks/watch');
 
-gulp.task("default", function() {
-	console.log("Hooray - you have created a new gulp task");
-});
 
-gulp.task("html", function() {
-	console.log("Something usefull for HTML");
-});
 
-gulp.task('styles', function() {
-	
-});
 
-gulp.task("watch", function() {
-	watch('./app/index.html', function() {
-		gulp.start('html');
-	});
 
-	watch('./app/assets/styles/**/*.css', function() {
-		gulp.start('styles');
-	});
-}); 
+
+
